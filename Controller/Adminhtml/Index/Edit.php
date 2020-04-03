046bf50efc1618de3d91fe13f49823d56179e8d9
Aashan Ghimire <ashanghimire10@gmail.com>
2020-04-03 17:53:58 +0545

    create controllers for crud operation

---
+++ Controller/Adminhtml/Index/Edit.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is a part of HighlightedCategories Magento 2 module.
+ *
+ * @module HighlightedCategories
+ * @author Ashan Ghimire <ashanghimire10@gmail.com>
+ * @copyright Ashan Ghimire, 2020
+ */
+
+namespace Aashan\HighlightedCategories\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Edit extends Action
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context  $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Returns edit page for category highlight.
+     *
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        $page = $this->resultPageFactory->create();
+        $page->getConfig()->getTitle()->prepend(__("Edit Highlighted Category"));
+        return $page;
+    }
+}
+
