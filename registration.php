cb0798891418645e680f6efff52615aef0875dc1
Aashan Ghimire <ashanghimire10@gmail.com>
2020-04-03 16:01:46 +0545

    setup new magento 2 module for highlighted categories

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is a part of HighlightedCategories Magento 2 module.
+ *
+ * @module HighlightedCategories
+ * @author Ashan Ghimire <ashanghimire10@gmail.com>
+ * @copyright Ashan Ghimire, 2020
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Aashan_HighlightedCategories',
+    __DIR__
+);
